3ea23dc243ece1d6d8ddceab957504ec407c04c6
meghal2017 <meghalgupta23@gmail.com>
2017-06-12 06:15:59 +0530

    Basic Changes

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/image2.jpg') {
+      myImage.setAttribute ('src','images/image1.jpg');
+    } else {
+      myImage.setAttribute ('src','images/image2.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
